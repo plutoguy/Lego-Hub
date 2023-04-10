2d4e9395202aee0ea41e7db5a14df4deb2ae089d
Lux14 <magnito14@pm.me>
2023-04-10 16:17:35 -0400

    Update ScriptLoader.lua

--- Config/ScriptLoader.lua
+++ Config/ScriptLoader.lua
@@ -14,3 +14,19 @@
         Filename: ScriptLoader.lua
         Authors: Lux14, pluto-guy
 ]]
+
+local SupportedGames = {
+        --// Coming Soon.
+}
+
+local Success, _ = pcall(function()
+        loadstring(game:HttpGet(SupportedGames[game.PlaceId]))()
+end)
+
+local Notification = {
+        Title = "Lego Hub",
+        Text = Success and "Script loaded! Thank you for choosing Lego Hub!" or "This game is not supported by Lego Hub!",
+        Duration = 10
+}
+
+game:GetService("StarterGui"):SetCore("SendNotification", Notification)

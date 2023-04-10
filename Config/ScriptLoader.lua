d3176c5fa29217cd85ce750c3dc4358d36b460c7
Lux14 <magnito14@pm.me>
2023-04-10 17:25:57 -0400

    Update ScriptLoader.lua

--- Config/ScriptLoader.lua
+++ Config/ScriptLoader.lua
@@ -12,7 +12,7 @@
         Free Lego Hacks | Modifying Roblox in a creative way since 2023
 
         Filename: ScriptLoader.lua
-        Authors: Lux14, pluto-guy
+        Authors: Lux14 & pluto-guy
 ]]--
 
 local SupportedGames = {
